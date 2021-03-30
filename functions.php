9911cbee13edc512b0d9a3fc2665d615ede2215e
emmatrain <emtrain7@gmail.com>
2021-03-30 07:16:36 -0300

    css styling added to output, header img added

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'dbconnection.php';
+
 /**
 * Retrieves the wines from the database
 *
@@ -14,9 +16,42 @@ $query->execute();
 return $query->fetchAll();
 }
 
+//Variables
+
+$db = getDb();
+$wines = getWines($db);
+
+/**
+ * Iterates over the db array and displays using HTML and CSS
+ *
+ * @param array $wines the data
+ * @return data in front end using echo
+ */
+function displayWines(array $wines) {
+    foreach ($wines as $wine) {
+        $wineName = $wine['wine-name'];
+        $origin = $wine['origin'];
+        $grape = $wine['grape'];
+        $style = $wine['style'];
+        $farming = $wine['farming'];
+        $abv = $wine['abv'];
+        $image = $wine['image-url'];
+        ?>
+
+        <div class="item">
+            <div class="header-wrap"><h3><?php echo $wineName ?></h3></div>
+            <img class="bottle" src="<?php echo $image?>">
+            <div class="stats-wrap">
+                <h4>Origin </h4><p><?php echo $origin ?></p><br>
+                <h4>Grape </h4><p><?php echo $grape ?></p><br>
+                <h4>Style </h4><p><?php echo $style ?></p><br>
+                <h4>Farming </h4><p><?php echo $farming ?></p><br>
+                <h4>ABV </h4><p><?php echo $abv ?>%</p><br>
+        </div>
+        </div>
+        <?php
+    } } ?>
 
 
 
 
-
-?>
--- index.php
+++ index.php
@@ -1,40 +1,38 @@
 <?php
 
-require_once 'dbconnection.php';
 require_once 'functions.php';
 
-
-
-
-$db = getDb();
-$allWines = getWines($db);
-echo '<pre>';
-var_dump($allWines);
-echo '</pre>';
 ?>
 
-<!---->
-<!--<!DOCTYPE html>-->
-<!---->
-<!--<html>-->
-<!---->
-<!--<head>-->
-<!---->
-<!--</head>-->
-<!---->
-<!--<body>-->
-<!---->
-<!--<header>-->
-<!--    <h1>Wine Collection</h1>-->
-<!--</header>-->
-<!---->
-<!--<body>-->
-<!---->
-<!---->
-<!---->
-<!---->
-<!---->
-<!--</body>-->
-<!---->
-<!---->
-<!--</html>-->
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="styles.css" type="text/css" />
+    <link rel="stylesheet" href="normalize.css" type="text/css" />
+    <link rel="stylesheet" href="https://use.typekit.net/kln6efb.css">
+</head>
+
+<header>
+
+<img class="header-img" src="images/header.png">
+
+</header>
+
+<body>
+
+    <div class="wine-box">
+        <?php echo displayWines($wines); ?>
+    </div>
+
+
+
+
+</body>
+
+
+</html>
